b1c5c28222a57c497ccfce1ed74fe02acedf30c9
YanTingLin <H34066131@gs.ncku.edu.tw>
2020-11-02 11:13:29 +0800

    feat: Add vue-socket.io

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import VueSocketIO from 'vue-socket.io'
 
 // mock api
 import '../mock'
@@ -9,6 +10,16 @@ import '../mock'
 // css
 import './styles/style.scss'
 Vue.config.productionTip = false
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://localhost:4000',
+  vuex: {
+    store,
+    actionPrefix: 'SOCKET_',
+    mutationPrefix: 'SOCKET_'
+  }
+}))
 
 new Vue({
   router,

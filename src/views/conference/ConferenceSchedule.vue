aab24a7a7d757f8d311744419485b380647828c8
YanTingLin <H34066131@gs.ncku.edu.tw>
2021-01-24 20:34:47 +0800

    feat: 新增id錯誤時的處理

--- src/views/conference/ConferenceSchedule.vue
+++ src/views/conference/ConferenceSchedule.vue
@@ -1,46 +1,52 @@
 <template>
   <div class="conference_schedule">
-    <div class="topic">議程</div>
-    <div class="schedule_list">
-      <div class="schedule_block">
-        <h4 class="schedule_block__title">一、正式開會</h4>
-      </div>
-      <div class="schedule_block">
-        <h4 class="schedule_block__title">二、主席報告</h4>
-      </div>
-      <div class="schedule_block">
-        <h4 class="schedule_block__title">三、行政報告</h4>
-      </div>
-      <div class="schedule_block">
-        <h4 class="schedule_block__title">四、議案與討論事項</h4>
-        <div class="schedule_block__detail">
-          <div
-            v-for="(proposal, index) in proposalList"
-            :key="proposal.proposalID"
-            class="case"
-          >
-            <router-link
-              :to="{
-                name: 'detail',
-                params: { proposalID: proposal.id }
-              }"
-              tag="div"
-              class="case__number"
-              >第{{ convertNumber(index + 1) }}案
-            </router-link>
-            <div class="case__proposer">{{ proposal.dept }}</div>
+    <div v-if="this.proposalList.length">
+      <div class="topic">議程</div>
+      <div class="schedule_list">
+        <div class="schedule_block">
+          <h4 class="schedule_block__title">一、正式開會</h4>
+        </div>
+        <div class="schedule_block">
+          <h4 class="schedule_block__title">二、主席報告</h4>
+        </div>
+        <div class="schedule_block">
+          <h4 class="schedule_block__title">三、行政報告</h4>
+        </div>
+        <div class="schedule_block">
+          <h4 class="schedule_block__title">四、議案與討論事項</h4>
+          <div class="schedule_block__detail">
+            <div
+              v-for="(proposal, index) in proposalList"
+              :key="proposal.proposalID"
+              class="case"
+            >
+              <router-link
+                :to="{
+                  name: 'detail',
+                  params: { proposalID: proposal.id }
+                }"
+                tag="div"
+                class="case__number"
+                >第{{ convertNumber(index + 1) }}案
+              </router-link>
+              <div class="case__proposer">{{ proposal.dept }}</div>
+            </div>
           </div>
         </div>
+        <div class="schedule_block">
+          <h4 class="schedule_block__title">五、臨時動議</h4>
+        </div>
+        <div class="schedule_block">
+          <h4 class="schedule_block__title">六、聲明與補述</h4>
+        </div>
+        <div class="schedule_block">
+          <h4 class="schedule_block__title">七、散會</h4>
+        </div>
       </div>
-      <div class="schedule_block">
-        <h4 class="schedule_block__title">五、臨時動議</h4>
-      </div>
-      <div class="schedule_block">
-        <h4 class="schedule_block__title">六、聲明與補述</h4>
-      </div>
-      <div class="schedule_block">
-        <h4 class="schedule_block__title">七、散會</h4>
-      </div>
+    </div>
+    <div v-else>
+      <p>查無此會議</p>
+      <router-link :to="{name: 'home'}" class="btn" tag="span">回到首頁</router-link>
     </div>
   </div>
 </template>
@@ -68,8 +74,14 @@ export default {
     ...mapActions([
       'setLodingStatus'
     ]),
+    ...mapActions({
+      setDelibrationInfo: 'delibration/setDelibrationInfo'
+    }),
     async getProposal (dID) {
       const response = await getProposals(dID)
+      if (response.data.length === 0) {
+        this.setDelibrationInfo({ semester: 0, period: 0, name: '無此會議' })
+      }
       this.proposalList = response.data
     },
     convertNumber (num) {
@@ -162,5 +174,12 @@ export default {
       }
     }
   }
+}
+
+.btn {
+  @include btn;
+  background-color: $primary;
+  font-size: $text_s;
+  padding: 5px 30px;
 }
 </style>
